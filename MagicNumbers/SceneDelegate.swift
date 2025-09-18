345ea5b8db90499727a3012b11bc1eb8921248aa
Oksana Dionisieva <atty.ltd@gmail.com>
2025-09-18 10:42:37 +0300

    Updated SceneDelegate with main controller

--- MagicNumbers/SceneDelegate.swift
+++ MagicNumbers/SceneDelegate.swift
@@ -11,10 +11,16 @@ import CoreData
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        let nav = UINavigationController(rootViewController: UIViewController())
+        let storyboard = UIStoryboard(name: "MainViewController", bundle: nil)
+        guard let mainVC = storyboard.instantiateViewController(identifier: "MainViewController") as? MainViewController else { return }
+
+        mainVC.context = AppDelegate.shared.persistentContainer.viewContext
+
+        let nav = UINavigationController(rootViewController: mainVC)
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = nav
         self.window = window
